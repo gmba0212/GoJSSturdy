8a28c987c9ee6ee185cad641e55fe905fe946d11
mb <gongminbae@naver.com>
2018-03-14 17:42:54 +0900

    xmlParshing예제 엘리먼트 삭제 제거 검색 자식으로추가 등등

--- javaScriptXMLParser/WebContent/js/javaScriptXMLParser.js
+++ javaScriptXMLParser/WebContent/js/javaScriptXMLParser.js
@@ -2,8 +2,85 @@
  * 
  */
 
-var hello={
+var xmlDoc = null;
+var xml={
 		sayHello:function(){
 			alert("helloWorld");
+		},
+		getXMLDoc_xmlStr:function(xmlstr){
+			if(window.DOMParser){
+				var parser = new DOMParser();
+				xmlDoc = parser.parseFromString(xmlstr,"text/xml");
+			}else{
+				xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+				xmlDoc.async=false;
+				xmlDoc.loadXML(xmlstr)
+			}
+			
+			return xmlDoc;
+		},
+		getXMLroot:function(xmlstr){
+			var root=null;
+			var xmldoc = this.getXMLDoc_xmlStr(xmlstr);
+			
+			root = xmldoc.documentElement;
+			return root;
+			
+		},
+		xmlTraVerse: function(xmlStr){
+			parser = new DOMParser();
+			xmlDoc = this.getXMLDoc_xmlStr(xmlStr);
+			
+			x = xmlDoc.documentElement.childNodes;
+			for(i=0 ; i<x.length;i++){
+				if(x[i].nodeName!=="#text"){
+					console.log(x[i].nodeName+" : "+x[i].innerHTML);
+				}
+			}
+		},
+		xmlSearchElement:function(xmlStr,str){
+			xmlDoc = this.getXMLDoc_xmlStr(xmlStr);
+			console.log(str);
+			var res=xmlDoc.getElementsByTagName(str);
+			console.log(res);
+		},	
+		createElement : function(xmlStr,text) {
+			xmlDoc = this.getXMLDoc_xmlStr(xmlStr);
+			console.log("before");
+			console.log((new XMLSerializer()).serializeToString(xmlDoc));
+			
+			var child = xmlDoc.createElement('member');
+			var textNode = xmlDoc.createTextNode(text);
+			child.appendChild(textNode);
+			xmlDoc.getElementsByTagName("members")[0].appendChild(child);
+			
+			console.log("after");
+			console.log((new XMLSerializer()).serializeToString(xmlDoc));
+			return child;
+		},
+		deleteMember :function(xmlStr,name){
+			var delNode=null;
+			xmlDoc = this.getXMLDoc_xmlStr(xmlStr);
+			console.log("before");
+			console.log((new XMLSerializer()).serializeToString(xmlDoc));
+
+			
+			var members=xmlDoc.getElementsByTagName("member");
+			console.log(members);
+			for( i = 0;i<members.length;i++){
+				if(members[i].textContent===name){
+					delNode = members[i];
+					
+				}
+			}
+			if(delNode === null){
+				console.log("noneMember.");
+			}else{
+				
+				delNode.parentNode.removeChild(delNode);
+				console.log("member :"+name+" delete")
+			}
+			console.log((new XMLSerializer()).serializeToString(xmlDoc));
+			
 		}
 }
--- javaScriptXMLParser/WebContent/jsp/view.jsp
+++ javaScriptXMLParser/WebContent/jsp/view.jsp
@@ -3,12 +3,25 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<script src="/js/javaScriptXMLParser.js"></script>
+<script src="../js/javaScriptXMLParser.js" charset="EUC-KR"></script>
 <meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
 <title>Insert title here</title>
 </head>
 <body>
+	<script>
+		var examXML='<?xml version="1.0" encoding="EUC-KR"?><girlgroup>  <name>여자친구</name>  <members>    <member>소원</member>    <member>예린</member>    <member>은하</member>    <member>유주</member>    <member>신비</member>    <member>엄지</member>  </members>  <albums>    <album order="EP 1집">Season of Glass</album>    <album order="EP 2집">Flower Bud</album>    <album order="EP 3집">Snowflake</album>    <album order="정규 1집">LOL</album>  </albums></girlgroup>';
+	</script>
 	
-	sayhello
+	sayhello<br>
+	<hr>
+	<br>
+	<br><hr>
+	TagName으로 찾기<input type="text" name="data" id="data" value="">
+	<input type="button" name ="search" value="searchStr" onclick='xml.xmlSearchElement(examXML,document.getElementById("data").value)'><br>
+	Traverse<input type="button" name="traverse" value="traverse" onclick='xml.xmlTraVerse(examXML)'><br>
+	CreateNode(멤버추가) 이름입력<input type ="text" id="name" value="">
+	<input type="button" name="CreateNode" value="CreateNode" onclick='xml.createElement(examXML,document.getElementById("name").value)'> <br>
+	DeleteNode(멤버삭제) 이름입력<input type ="text" id="deleteMember" value="">
+	<input type="button" name="deleteMember" value="deleteMember" onclick='xml.deleteMember(examXML,document.getElementById("deleteMember").value)'> <br>
 </body>
 </html>
